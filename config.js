1b90d7536701f5e70cd671c558c3ae578127dce0
Lucas <54530692+DuuyLocc@users.noreply.github.com>
2021-06-03 14:15:08 +0700

    why

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     introTitle: 'Mèo ơi Mèo à!',
     introDesc: `Trái đất vốn lạ thường
-                Mà sao em cứ đi nhầm đường
-                Lạc vào tim anh lẻ loi
-                Đằng sau chữ yêu đây là thương`,
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Chào mừng em đến với thế giới của anh',
     title:    'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc:     'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
