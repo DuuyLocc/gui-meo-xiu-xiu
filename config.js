45a63a012cd934c759769b832c9d8a7c942a7998
Lucas <54530692+DuuyLocc@users.noreply.github.com>
2021-06-03 13:40:22 +0700

    ...

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Chào mừng em đến với thế giới của anh',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnYes: 'Em đồng ý bên cạnh anh <3',
+    btnNo: 'Không đâu Anh à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại chọn anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.messenger.com/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messDesc: 'Mãi bên nhau em nhé! ^_^.',
+    btnAccept: 'Dạ Vâng anh <3',
+    messLink: 'https://www.messenger.com/t/100005948341852' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
